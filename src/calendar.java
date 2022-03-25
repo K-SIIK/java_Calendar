d9cb8e9bc9cfaf33b3ea481062649fa55e3db4d3
K-SIIK <kangchangik32@gmail.com>
2022-03-25 16:22:50 +0900

    calendar image

--- src/calendar.java
+++ src/calendar.java
@@ -1,5 +1,10 @@
 public class calendar {
     public static void main(String[] args) {
-        System.out.println("hello java");
+        System.out.println("\t일\t월\t화\t수\t목\t금\t토");
+        System.out.println("\t--------------------------");
+        System.out.println("\t1\t2\t3\t4\t5\t6\t7");
+        System.out.println("\t8\t9\t10\t11\t12\t13\t14");
+        System.out.println("\t15\t16\t17\t18\t19\t20\t21");
+        System.out.println("\t22\t23\t24\t25\t26\t27\t28");
     }
 }

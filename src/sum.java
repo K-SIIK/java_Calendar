724b4113399890797cbfb2a262c208d8b264b70b
K-SIIK <kangchangik32@gmail.com>
2022-03-26 12:40:31 +0900

    sum updated

--- src/sum.java
+++ src/sum.java
@@ -2,13 +2,19 @@ import java.util.Scanner;
 
 public class sum {
     public static void main(String[] args) {
-        System.out.println("두 수를 입력하세요.");
-        Scanner sc = new Scanner(System.in);
-        String inputValue = sc.nextLine();
-        String[] splitValue = inputValue.split(" ");
-        int num1 = Integer.parseInt(splitValue[0]);
-        int num2 = Integer.parseInt(splitValue[1]);
-        System.out.printf("%d와 %d의 합은 %d입니다.",num1, num2, num1 + num2);
+        // 입력
+        System.out.println("두 수 입력 : ");
+        Scanner scanner = new Scanner(System.in);
+        String a, b;
+        a = scanner.next();
+        b = scanner.next();
+        System.out.println(a + "+" + b);
 
+        // 출력
+        int num1, num2;
+        num1 = Integer.parseInt(a);
+        num2 = Integer.parseInt(b);
+        int result = num1 + num2;
+        System.out.println(result);
     }
 }

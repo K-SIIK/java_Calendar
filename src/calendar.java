691a5dd3052434a54e98ae6a567eda3e9053ae51
K-SIIK <kangchangik32@gmail.com>
2022-03-26 15:06:56 +0900

    maxdays print several times

--- src/calendar.java
+++ src/calendar.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class calendar {
@@ -9,16 +10,21 @@ public class calendar {
         System.out.println("\t15\t16\t17\t18\t19\t20\t21");
         System.out.println("\t22\t23\t24\t25\t26\t27\t28");
 
-        //월을 입력하면 해당 월의 최대 일수 출력
-        System.out.println("월 입력 : ");
-        Scanner sc = new Scanner(System.in);
-        int month = sc.nextInt();
-
         // 최대일수 별로 Array로 정리
         int[] maxDays = {31,28,31,30,31,30,31,31,30,31,30,31};
 
-        System.out.printf("%d월은 %d일까지 있습니다.",month, maxDays[month - 1]);
+        //월을 입력하면 해당 월의 최대 일수 출력
+        System.out.println("입력 횟수 : ");
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
 
+        for (int i=0; i<cnt; i++) {
+            System.out.println("달을 입력하세요.");
+            int month = sc.nextInt();
+            System.out.printf("%d월은 %d일 까지 있습니다.\n",month, maxDays[month - 1]);
+
+        }
+        System.out.println("Bye~");
 
 
     }

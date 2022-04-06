bde287aa96a719b2328d77e8bdbc165545090118
K-SIIK <kangchangik32@gmail.com>
2022-04-06 12:24:03 +0900

    add function that register and search plan

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,12 +1,36 @@
-public class Calendar {
+// Calendar
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+public class Calendar {
     public static final int[] MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     public static final int[] LEAP_MAX_DAYS = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+    private HashMap <Date, String> planMap;
+
+    public Calendar() {
+        planMap = new HashMap<Date, String>();
+    }
+
+    public void registerPlan(String strDate, String plan) throws ParseException {
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+        planMap.put(date, plan);
+    }
+
+    public String searchPlan(String strDate) throws ParseException {
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+        String plan = planMap.get(date);
+        return plan;
+    }
+
     public int maxDaysOfMonth(int year, int month) {
-        if ((year % 4 ==0 && year % 100 != 0) || year % 400 == 0) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             return LEAP_MAX_DAYS[month];
         }
+
         else {
             return MAX_DAYS[month];
         }
@@ -17,48 +41,63 @@ public class Calendar {
         System.out.println("\tSU\tMO\tTU\tWE\tTH\tFR\tSA");
         System.out.println("\t--------------------------");
 
-        int weekday = getWeekday(year, month);
+
+        int maxDay = maxDaysOfMonth(year, month);
+        int weekday = getWeekdays(year, month);
 
         for (int i=0; i<weekday; i++) {
             System.out.print("\t");
         }
 
         for (int j=1; j<=7-weekday; j++) {
-            System.out.printf("\t%d",j);
+            System.out.printf("\t%d", j);
         }
         System.out.println();
 
-        int maxDays = maxDaysOfMonth(year, month);
         int cnt = 7 - weekday;
-        if (weekday == 0) {
+        if (cnt == 7) {
             cnt = 0;
         }
 
-        for (int k=8-weekday; k<=maxDays; k++) {
+        for (int k=8-weekday; k<=maxDay; k++) {
             System.out.printf("\t%d",k);
-
             if (k % 7 == cnt) {
                 System.out.println();
             }
         }
         System.out.println();
+        System.out.println();
     }
 
+    public int getWeekdays(int year, int month) {
+        int initialDay = 1; // 1년 1월 1일이 월요일 기준 잡음
 
-    public int getWeekday(int year, int month) {
-
-        int yearSum = 1; // 1년 1월 1일은 월요일 기준
+        // 년
+        int yearSum = 0;
         for (int i=1; i<year; i++) {
             for (int j=1; j<=12; j++) {
                 yearSum += maxDaysOfMonth(i,j);
             }
         }
+
+        // 월
         int monthSum = 0;
         for (int k=1; k<month; k++) {
-            monthSum += maxDaysOfMonth(year,k);
+            monthSum += maxDaysOfMonth(year, k);
         }
 
-        return (yearSum + monthSum) % 7;
+        return (initialDay + yearSum + monthSum) % 7;
     }
 
+    public static void main(String[] args) throws ParseException {
+        Calendar cal = new Calendar();
+
+        System.out.println(cal.getWeekdays(1970,1) == 4);
+        System.out.println(cal.getWeekdays(1971,1) == 5);
+        System.out.println(cal.getWeekdays(1972,1) == 6);
+        System.out.println(cal.getWeekdays(1973,1) == 1);
+        System.out.println(cal.getWeekdays(1974,1) == 2);
+        cal.registerPlan("2022-04-05","Let's have a dinner");
+        System.out.println(cal.searchPlan("2022-04-05").equals("Let's have a dinner"));
+    }
 }
--- src/Prompt.java
+++ src/Prompt.java
@@ -1,45 +1,105 @@
+//Prompt
+
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class Prompt {
-//    public int parseWeek(String week) {
-//        switch (week) {
-//            case "mo" : return 1;
-//            case "tu" : return 2;
-//            case "we" : return 3;
-//            case "th" : return 4;
-//            case "fr" : return 5;
-//            case "sa" : return 6;
-//            default : return 0;
-//        }
-//    }
+    public void printMenu() {
+        System.out.println("+------------------+");
+        System.out.println("|   1. 일정 등록 ");
+        System.out.println("|   2. 일정 검색 ");
+        System.out.println("|   3. 달력 보기 ");
+        System.out.println("|   h. 도움말  q. 종료 ");
+        System.out.println("+------------------+");
+    }
 
-    public void runPrompt() {
-        Scanner scanner = new Scanner(System.in);
-        Calendar cal = new Calendar();
-
-        while (true) {
-            System.out.println("년을 입력하세요.");
-            System.out.print("year> ");
-            int year = scanner.nextInt();
-            if (year < 1) {
-                System.out.println("bye");
-                break;
-            }
-
-            System.out.println("월을 입력하세요.");
-            System.out.print("month> ");
-            int month = scanner.nextInt();
-
-            if (month < 1 || month > 12) {
-                System.out.println("다시 입력");
-                continue;
-            }
-
-            cal.printCalendar(year,month);
+    public int parseInt(String week) {
+        switch(week) {
+            case "su" : return 0;
+            case "mo" : return 1;
+            case "tu" : return 2;
+            case "we" : return 3;
+            case "th" : return 4;
+            case "fr" : return 5;
+            case "sa" : return 6;
+            default: return 0;
         }
     }
 
-    public static void main(String[] args) {
+    public void runPrompt() throws ParseException {
+        printMenu();
+
+        Scanner scanner = new Scanner(System.in);
+        Calendar cal = new Calendar();
+
+        boolean isLoop = true;
+        while(isLoop) {
+            System.out.println("명령 (1, 2, 3, h, q)");
+            String cmd = scanner.next();
+            switch(cmd) {
+                case "1" :
+                    cmdRegister(scanner, cal);
+                    break;
+                case "2" :
+                    cmdSearch(scanner, cal);
+                    break;
+                case "3" :
+                    cmdCal(scanner, cal);
+                case "h" :
+                    printMenu();
+                case "q" :
+                    isLoop = false;
+                    break;
+            }
+        }
+        System.out.println("Thank you. Bye");
+        scanner.close();
+
+    }
+
+    private void cmdCal(Scanner s, Calendar c) {
+
+        System.out.println("년을 입력하세요.");
+        System.out.print("year> ");
+        int year = s.nextInt();
+
+        System.out.println("월을 입력하세요.");
+        System.out.print("month> ");
+        int month = s.nextInt();
+        if (month < 1 || month > 12) {
+            System.out.println("잘못 입력하셨습니다.");
+            return;
+        }
+        c.printCalendar(year, month);
+    }
+
+    private void cmdSearch(Scanner s, Calendar c) {
+        System.out.println("[일정 검색]");
+        System.out.println("날짜를 입력해 주세요.(yyyy-MM-dd).");
+        String date = s.next();
+        String plan = "";
+        try {
+            plan = c.searchPlan(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            System.err.println("일정 검색 중 오류가 발생하였습니다.");
+        }
+        System.out.println(plan);
+    }
+
+    private void cmdRegister(Scanner s, Calendar c) throws ParseException {
+        System.out.println("[새 일정 등록]");
+        System.out.println("날짜를 입력해 주세요.(yyyy-MM-dd).");
+        String date = s.next();
+        String text = "";
+        s.nextLine();
+        System.out.println("일정을 입력해 주세요.");
+        text = s.nextLine();
+
+        c.registerPlan(date, text);
+    }
+
+    public static void main(String[] args) throws ParseException {
         Prompt p = new Prompt();
         p.runPrompt();
     }
